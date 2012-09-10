e5abe839397e3f4104a823699b08ded7fee36219
Roger M <roger.kang.mo@gmail.com>
2012-09-10 00:26:55 -0400

    Added HashMap
    What was I thinking?

--- Parsing English Values/EnglishParser.java
+++ Parsing English Values/EnglishParser.java
@@ -1,74 +1,70 @@
 import java.io.*;
 import java.util.*;
 
+
+// http://www.reddit.com/r/dailyprogrammer/comments/zfeb2/9062012_challenge_96_intermediate_parsing_english/
+
+
 public class EnglishParser {
 
-	private List<Pair> values;
+	private Map<String, Integer> values;
+	private Map<String, Integer> magnitudes;
 
 	public EnglishParser(){
-		values = new ArrayList<Pair>();
+		values = new HashMap<String, Integer>();
+		magnitudes = new HashMap<String, Integer>();
+
+		setupPairs();
 	}
+
+	private int parsePart(String part){
+		int returnVal = 0;
+
+		String[] pieces = part.split("-");
+
+		returnVal = values.get(pieces[0]);
+
+		if(pieces[1] != null){
+			returnVal *= magnitudes.get(pieces[1]);
+		}
+		
+		return returnVal;	
+
+	}
+
+	//Four-thousand twenty seven
+	//Twenty-thousand two-hundred and forty-four
 
 	private void setupPairs(){
-		values.add(new Pair(1, "one"));
-		values.add(new Pair(2,"two"));
-		values.add(new Pair(3,"three"));
-		values.add(new Pair(4,"four"));
-		values.add(new Pair(5,"five"));
-		values.add(new Pair(6,"six"));
-		values.add(new Pair(7,"seven"));
-		values.add(new Pair(8,"eight"));
-		values.add(new Pair(9,"nine"));
-		values.add(new Pair(10,"ten"));
-		values.add(new Pair(11,"eleven"));
-		values.add(new Pair(12,"twelve"));
-		values.add(new Pair(13,"thirteen"));
-		values.add(new Pair(14,"fourteen"));
-		values.add(new Pair(15,"fifteen"));
-		values.add(new Pair(16,"sixteen"));
-		values.add(new Pair(17,"seventeen"));
-		values.add(new Pair(18,"eighteen"));
-		values.add(new Pair(19,"nineteen"));
-		values.add(new Pair(20,"twenty"));
-		values.add(new Pair(30,"thirty"));
-		values.add(new Pair(40,"fourty"));
-		values.add(new Pair(50,"fifty"));
-		values.add(new Pair(60,"sixty"));
-		values.add(new Pair(70,"seventy"));
-		values.add(new Pair(80,"eighty"));
-		values.add(new Pair(90,"ninety"));
-		values.add(new Pair(100,"hundred", true));
-		values.add(new Pair(1000,"thousand", true));
-	}
-
-	public class Pair {
-		
-		private int value;
-		private String english;
-		private boolean isMagnitude;
-
-		public Pair(int val, String num){
-			value = val;
-			english = num;
-			isMagnitude = false;
-		}
-
-		public Pair(int val, String num, boolean isMag){
-			value = val;
-			english = num;
-			isMagnitude = isMag;
-		}
-
-		public String getEnglish(){
-			return english;
-		}
-
-		public int getValue(){
-			return value;
-		}
-
-		public boolean getIsMagnitude(){
-			return isMagnitude;
-		}
+		values.put("one", 1);
+		values.put("two", 2);
+		values.put("three", 3);
+		values.put("four", 4);
+		values.put("five", 5);
+		values.put("six", 6);
+		values.put("seven", 7);
+		values.put("eight", 8);
+		values.put("nine", 9);
+		values.put("ten", 10);
+		values.put("eleven", 11);
+		values.put("twelve", 12);
+		values.put("thirteen", 13);
+		values.put("fourteen", 14);
+		values.put("fifteen", 15);
+		values.put("sixteen", 16);
+		values.put("seventeen", 17);
+		values.put("eighteen", 18);
+		values.put("nineteen", 19);
+		values.put("twenty", 20);
+		values.put("thirty", 30);
+		values.put("fourty", 40);
+		values.put("fifty", 50);
+		values.put("sixty", 60);
+		values.put("seventy", 70);
+		values.put("eighty", 80);
+		values.put("ninety", 90);
+		magnitudes.put("hundred", 100);
+		magnitudes.put("thousand", 1000);
+		magnitudes.put("million", 1000000);
 	}
 }
